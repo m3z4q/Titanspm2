e5392046ed258280acfd0a9d5d7fe567cc5e3ae7
m3z4q <titaneraontop@gmail.com>
2025-12-25 22:46:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,11 @@
 import asyncio
-import logging
 from telegram import Update
 from telegram.ext import Application, CommandHandler, ContextTypes
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-    level=logging.INFO
-)
 
 BOT_TOKEN = "8487272111:AAEkEnUUuOg-YuJxyGS0z9lqGhhWn6HPokU"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Bot started and running fine!")
+    await update.message.reply_text("Bot chal raha hai!")
 
 async def main():
     app = Application.builder().token(BOT_TOKEN).build()
